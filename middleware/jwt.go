e45e253c39bfdf190c10777307222c6f858b8df9
Muhamad Abdul Aziz <azizgimboell@gmail.com>
2025-01-18 23:54:37 +0700

    reorder jwt auth validation

--- middleware/jwt.go
+++ middleware/jwt.go
@@ -23,6 +23,16 @@ func JWTAuth() gin.HandlerFunc {
 			return
 		}
 
+		tokenString := auth[7:]
+		claims, err := utils.ValidateJWT(tokenString)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+			c.Abort()
+			return
+		}
+
+		c.Set("claims", claims)
+
 		if c.Request.Method == http.MethodPost || c.Request.Method == http.MethodPut || c.Request.Method == http.MethodPatch {
 			contentType := c.GetHeader("Content-Type")
 
@@ -35,22 +45,13 @@ func JWTAuth() gin.HandlerFunc {
 				}
 			default:
 				if contentType != "application/json" {
-					c.JSON(http.StatusBadRequest, gin.H{"error": "Missing content-type"})
+					c.JSON(http.StatusBadRequest, gin.H{"error": "Content-Type must be application/json"})
 					c.Abort()
 					return
 				}
 			}
 		}
 
-		auth = auth[7:]
-		claims, err := utils.ValidateJWT(auth)
-		if err != nil {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
-			c.Abort()
-			return
-		}
-
-		c.Set("claims", claims)
 		c.Next()
 	}
 }
